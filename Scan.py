0cc16987cf95b287ac37e67b75ba034cfbf84024
Doc-Hack <the.doc.hack@gmail.com>
2017-12-13 16:09:52 -0600

    Add files via upload

---
+++ Scan.py
@@ -0,0 +1,7 @@
+from libnmap.parser import NmapParser
+from libnmap.process import NmapProcess
+from openpyxl import Workbook
+from openpyxl.compat import range
+from openpyxl.utils import get_column_letter
+import datetime
+import manuf
